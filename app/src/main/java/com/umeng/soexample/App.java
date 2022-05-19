c9c2ad9627ed667c37363d1fac804a86e0e41c9a
yujie <yj131525@umeng.com>
2022-05-19 14:56:28 +0800

    1.增加qt-common包离线依赖。2.增加qt gradle插件依赖。

--- app/src/main/java/com/umeng/soexample/App.java
+++ app/src/main/java/com/umeng/soexample/App.java
@@ -3,6 +3,8 @@ package com.umeng.soexample;
 
 import android.app.Application;
 
+import com.quick.qt.analytics.QtTrackAgent;
+import com.quick.qt.commonsdk.QtConfigure;
 import com.tencent.tauth.Tencent;
 import com.umeng.commonsdk.UMConfigure;
 import com.umeng.message.PushAgent;
@@ -23,11 +25,18 @@ public class App extends Application {
 
         //设置LOG开关，默认为false
         UMConfigure.setLogEnabled(true);
+        QtConfigure.setCustomDomain("https://log-api.aplus.emas-poc.com", null);
+        //QT 统计打开调试日志
+        QtConfigure.setLogEnabled(true);
+
         //解决推送消息显示乱码的问题
         PushAgent.setup(this, "59892f08310c9307b60023d0", "669c30a9584623e70e8cd01b0381dcb4");
         //友盟预初始化
         UMConfigure.preInit(getApplicationContext(),"59892f08310c9307b60023d0","Umeng");
-
+        QtConfigure.preInit(this, "64632267", "QuickTrack");
+        QtTrackAgent.setAutoEventEnabled(true);
+        QtTrackAgent.enableFragmentPageCollection(true);
+        QtConfigure.setProcessEvent(true);//支持多进程 打点.默认不支持
         /**
          * 打开app首次隐私协议授权，以及sdk初始化，判断逻辑请查看SplashTestActivity
          */
--- app/src/main/java/com/umeng/soexample/UmInitConfig.java
+++ app/src/main/java/com/umeng/soexample/UmInitConfig.java
@@ -11,6 +11,7 @@ import android.util.Log;
 import android.widget.RemoteViews;
 import android.widget.Toast;
 
+import com.quick.qt.commonsdk.QtConfigure;
 import com.umeng.analytics.MobclickAgent;
 import com.umeng.commonsdk.UMConfigure;
 import com.umeng.message.IUmengRegisterCallback;
@@ -38,7 +39,7 @@ public class UmInitConfig {
         //初始化组件化基础库, 统计SDK/推送SDK/分享SDK都必须调用此初始化接口
         UMConfigure.init(context, "59892f08310c9307b60023d0", "Umeng", UMConfigure.DEVICE_TYPE_PHONE,
                 "669c30a9584623e70e8cd01b0381dcb4");
-
+        QtConfigure.init(context, "64632267", "QuickTrack", QtConfigure.DEVICE_TYPE_PHONE, null);
         String FileProvider = "com.umeng.soexample.fileprovider";
         PlatformConfig.setWeixin("wxdc1e388c3822c80b", "3baf1193c85774b3fd9d18447d76cab0");
         PlatformConfig.setWXFileProvider(FileProvider);
