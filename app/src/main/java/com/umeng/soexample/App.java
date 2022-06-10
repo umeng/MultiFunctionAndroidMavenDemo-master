b909c70afcf5e4086484ee52afbfe94d206f5ac2
ty <>
2022-06-10 17:27:08 +0800

    升级Push组件6.5.2

--- app/src/main/java/com/umeng/soexample/App.java
+++ app/src/main/java/com/umeng/soexample/App.java
@@ -23,11 +23,8 @@ public class App extends Application {
 
         //设置LOG开关，默认为false
         UMConfigure.setLogEnabled(true);
-        //解决推送消息显示乱码的问题
-        PushAgent.setup(this, "59892f08310c9307b60023d0", "669c30a9584623e70e8cd01b0381dcb4");
         //友盟预初始化
         UMConfigure.preInit(getApplicationContext(),"59892f08310c9307b60023d0","Umeng");
-
         /**
          * 打开app首次隐私协议授权，以及sdk初始化，判断逻辑请查看SplashTestActivity
          */
